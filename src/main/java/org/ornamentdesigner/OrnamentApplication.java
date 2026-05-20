6b9664feae5bfaf8d0509d2a65eead94bd00a71d
Vadim Dudar <v.dudar@ukma.edu.ua>
2026-05-20 20:07:51 +0300

    added possibility to paint

--- src/main/java/org/ornamentdesigner/OrnamentApplication.java
+++ src/main/java/org/ornamentdesigner/OrnamentApplication.java
@@ -18,10 +18,31 @@ public class OrnamentApplication extends Application {
     private static final int HEIGHT = 1000;
     private static final int CELLS = 20;
 
+    private final Rectangle[][] rectangles = new Rectangle[CELLS][CELLS];
+    private final Color[][] colors = new Color[CELLS][CELLS];
+    private Color currentColor = Color.RED;
+
     @Override
     public void start(Stage stage) throws IOException {
         var root = new Group();
 
+        createCanvas(root);
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setResizable(false);
+        stage.setTitle("Ornament Designer - Vadim Dudar");
+        stage.getIcons().add(new Image(
+                Objects.requireNonNull(OrnamentApplication.class.getResourceAsStream("icon.png"))
+        ));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void main() {
+        launch();
+    }
+
+    private void createCanvas(Group root) {
         int dimension = Math.min(WIDTH, HEIGHT) - 100;
         final int marginRectangle = 50;
         var rectangle = new Rectangle(marginRectangle, marginRectangle, dimension, dimension);
@@ -39,19 +60,28 @@ public class OrnamentApplication extends Application {
             vertical.setStroke(Color.LIGHTGRAY);
             vertical.setStrokeWidth(1);
             root.getChildren().add(vertical);
+
+            for (int j = 0; j < CELLS; j++) {
+                var cell = new Rectangle(
+                        marginRectangle + i * dimension / CELLS,
+                        marginRectangle + j * dimension / CELLS,
+                        dimension / CELLS, dimension / CELLS
+                );
+                cell.setFill(Color.TRANSPARENT);
+                rectangles[i][j] = cell;
+                colors[i][j] = Color.TRANSPARENT;
+                cell.setOnMousePressed(event -> {
+                    int x = (int) ((cell.getX() - marginRectangle) / (dimension / CELLS));
+                    int y = (int) ((cell.getY() - marginRectangle) / (dimension / CELLS));
+                    if (event.isControlDown()) {
+                        colors[x][y] = Color.TRANSPARENT;
+                    } else {
+                        colors[x][y] = colors[x][y].equals(Color.TRANSPARENT) ? currentColor : Color.TRANSPARENT;
+                    }
+                    cell.setFill(colors[x][y]);
+                });
+                root.getChildren().add(cell);
+            }
         }
-
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-        stage.setResizable(false);
-        stage.setTitle("Ornament Designer - Vadim Dudar");
-        stage.getIcons().add(new Image(
-                Objects.requireNonNull(OrnamentApplication.class.getResourceAsStream("icon.png"))
-        ));
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void main() {
-        launch();
     }
 }
